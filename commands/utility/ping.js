786ee5b909b79d5af9e6a862dd868031550bb605
Vinnicius Magione <vinniciussoares@hotmail.com>
2026-03-17 20:01:52 -0300

    Create reload command
    Create reload command

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
+    cooldown: 5,
     data: new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong!'),
     async execute(interaction) {
         await interaction.reply('Pong!');
--- index.js
+++ index.js
@@ -3,23 +3,16 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits, MessageFlags } = require('discord.js');
 const { token } = require('./config.json');
+
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-// When the client is ready, run this code (only once).
-// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
-// It makes some properties non-nullable.
-client.once(Events.ClientReady, (readyClient) => {
-    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-});
-// Log in to Discord with your client's token
-client.login(token);
 
 
 client.commands = new Collection();
 
-
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
+
 for (const folder of commandFolders) {
     const commandsPath = path.join(foldersPath, folder);
     const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
@@ -35,28 +28,18 @@ for (const folder of commandFolders) {
     }
 }
 
-client.on(Events.InteractionCreate, async (interaction) => {
 
-    if (!interaction.isChatInputCommand()) return;
-    const command = interaction.client.commands.get(interaction.commandName);
-    if (!command) {
-        console.error(`No command matching ${interaction.commandName} was found.`);
-        return;
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith('.js'));
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
     }
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-        if (interaction.replied || interaction.deferred) {
-            await interaction.followUp({
-                content: 'There was an error while executing this command!',
-                flags: MessageFlags.Ephemeral,
-            });
-        } else {
-            await interaction.reply({
-                content: 'There was an error while executing this command!',
-                flags: MessageFlags.Ephemeral,
-            });
-        }
-    }
-});
+}
+
+// Log in to Discord with your client's token
+client.login(token);
